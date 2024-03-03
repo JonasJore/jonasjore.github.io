cb0ea829c0b2906e650333843871e21ebf7f4545
Jonas <15245356+JonasJore@users.noreply.github.com>
2024-03-03 22:17:15 +0100

    🚑 Fixed formatting error

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
+import React from 'react'
 import './App.css'
 
 function App(): JSX.Element {
-  return (
-    <>
-      <section>
-        <h1>jonasjore.github.io</h1>
-        <p>Hello! My name is Jonas, i am a web developer</p>
-        <p>
-          I am passionate about my craft and to help my clients build suitable
-          solutions based on their needs
-        </p>
-        <h2>Get in touch with me</h2>
-        <span>email: </span>
-        <a href='mailto:jonas.jore@gmail.com'>jonas.jore@gmail.com</a>
-        <p>tlf: +47 95 01 515</p>
-        <span>Github: </span>
-        <a href='https://www.github.com/jonasjore'>@JonasJore</a>
-      </section>
-    </>
-  )
+    return (
+        <>
+            <section>
+                <h1>jonasjore.github.io</h1>
+                <p>Hello! My name is Jonas, i am a web developer</p>
+                <p>
+                    I am passionate about my craft and to help my clients build suitable solutions
+                    based on their needs
+                </p>
+                <h2>Get in touch with me</h2>
+                <span>email: </span>
+                <a href='mailto:jonas.jore@gmail.com'>jonas.jore@gmail.com</a>
+                <p>tlf: +47 95 01 515</p>
+                <span>Github: </span>
+                <a href='https://www.github.com/jonasjore'>@JonasJore</a>
+            </section>
+        </>
+    )
 }
 
 export default App
